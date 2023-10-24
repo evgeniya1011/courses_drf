e16b3fb77fd449291d49e6b2696abf16a1dd8ff5
evgeniya1011 <zhdavydova@mail.ru>
2023-10-24 19:43:02 +0300

    fix (hw24.2)

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from courses.serializers import PaymentsSerializer
 from users.models import User
 
 
 class UserSerializers(serializers.ModelSerializer):
+    payment = PaymentsSerializer(source='payment_set', many=True)
+
     class Meta:
         model = User
         fields = '__all__'
